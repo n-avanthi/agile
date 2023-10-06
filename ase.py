14692213918bac3171ea521bab13ee643228f783
Avanthi Narasingu <narasinguavanthi@gmail.com>
2023-10-06 16:37:44 +0530

    My first version

---
+++ ase.py
@@ -0,0 +1,12 @@
+"""
+This module demonstrates the use of constants and multiplication.
+"""
+
+# Initialize constant 'A' with value 3
+A = 3
+
+# Initialize constant 'B' with value 5
+B = 5
+
+# Print the product of 'A' and 'B'
+print(A * B)  # Output: 15
